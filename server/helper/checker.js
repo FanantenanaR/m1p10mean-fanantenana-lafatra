730a585763778bd5aa48e6b5fb66e104033a8144
FanantenanaR <80602341+FanantenanaR@users.noreply.github.com>
2023-01-19 14:49:02 +0300

    Add controle unitaire formulaire inscription

--- server/helper/checker.js
+++ server/helper/checker.js
@@ -4,28 +4,52 @@ const isDateValid = (daty) => {
     return moment(daty).isValid();
 };
 
+const IS_EMPTY_STRING = "String";
+const IS_VALID_DATE = "Date";
+const IS_VALID_NUMBER = "Number";
+const IS_VALID_INTEGER = "Integer";
+const IS_VALID_FLOAT = "Float";
+const IS_VALID_EMAIL = "email";
+
+/**
+ *
+ * @param {string | Date | Number } value The value we need to check
+ * @param {string} valueType The type to check.
+ * @returns `true` if
+ */
 const isInputValid = (value, valueType) => {
+
     if (value === undefined) {
         return false;
     }
-    if (valueType === "String") {
-        return !value || value.trim().length === 0;
+    if (valueType === IS_EMPTY_STRING) {
+        return value !== "" || value.trim().length !== 0;
     }
-    if (valueType === "Date") {
+    if (valueType === IS_VALID_DATE) {
         return isDateValid(value);
     }
-    if (valueType === "Number") {
-        return !isNaN(value) && isFinite(value);
+    if (valueType === IS_VALID_NUMBER) {
+        return !(isNaN(value)) && isFinite(value);
     }
-    if (valueType === "Integer") {
-        return !isNaN(value) && Number.isSafeInteger();
+    if (valueType === IS_VALID_INTEGER) {
+        return !(isNaN(value)) && Number.isSafeInteger();
     }
-    if (valueType === "Float") {
-        return !isNaN(value) && Number.isFinite(value);
+    if (valueType === IS_VALID_FLOAT) {
+        return !(isNaN(value)) && Number.isFinite(value);
+    }
+    if (valueType === IS_VALID_EMAIL) {
+        const conditionMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        return value.match(conditionMail);
     }
     return true;
 };
 
 module.exports = {
-    isInputValid
+    isInputValid,
+    IS_EMPTY_STRING,
+    IS_VALID_DATE,
+    IS_VALID_NUMBER,
+    IS_VALID_INTEGER,
+    IS_VALID_FLOAT,
+    IS_VALID_EMAIL
 };
--- server/services/identification.js
+++ server/services/identification.js
@@ -3,24 +3,126 @@ const Client = require('../model/client');
 
 const clientInscription = (request, response) => {
     const nom = request.body.nom;
-    if (!checker.isInputValid(nom, "String")) {
+    if (!checker.isInputValid(nom, checker.IS_EMPTY_STRING)) {
         const error = {
             "status": 500,
-            "message": "Veuillez remplir le champs 'nom'",
-            "errorType": "MissingField"
+            "message": "Veuillez remplir le champs 'nom'.",
+            "errorType": "InvalidField",
+            "field": "nom"
         };
         response.status(500).send(error);
+        return;
     }
     const prenom = request.body.prenom;
+    if (!checker.isInputValid(prenom, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'prenom'.",
+            "errorType": "InvalidField",
+            "field": "prenom"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const dateNaissance = request.body.dateNaissance;
+    if (!checker.isInputValid(dateNaissance, checker.IS_VALID_DATE)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir une date valide dans le champs 'date de naissance'.",
+            "errorType": "InvalidField",
+            "field": "dateNaissance"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const tel = request.body.tel;
+    if (!checker.isInputValid(tel, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'numero de telephone'.",
+            "errorType": "InvalidField",
+            "field": "tel"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const email = request.body.email;
+    if (!checker.isInputValid(email, checker.IS_VALID_EMAIL)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'email' de la forme 'abc@example.com'.",
+            "errorType": "InvalidField",
+            "field": "email"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const adresse = request.body.adresse;
+    if (!checker.isInputValid(adresse, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'adresse'.",
+            "errorType": "InvalidField",
+            "field": "adresse"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const numerocin = request.body.numerocin;
+    if (!checker.isInputValid(numerocin, checker.IS_VALID_INTEGER)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'Numero CIN'.",
+            "errorType": "InvalidField",
+            "field": "numerocin"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const dateDelivrance = request.body.dateDelivrance;
+    if (!checker.isInputValid(dateDelivrance, checker.IS_VALID_DATE)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir une date valide dans le champs 'date de delivrance'.",
+            "errorType": "InvalidField",
+            "field": "dateDelivrance"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const lieuDelivrance = request.body.lieuDelivrance;
+    if (!checker.isInputValid(nom, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'Lieu de délivrance'.",
+            "errorType": "InvalidField",
+            "field": "lieuDelivrance"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const login = request.body.login;
+    if (!checker.isInputValid(login, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'login'.",
+            "errorType": "InvalidField",
+            "field": "login"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const mdp = request.body.mdp;
+    if (!checker.isInputValid(nom, checker.IS_EMPTY_STRING)) {
+        const error = {
+            "status": 500,
+            "message": "Veuillez remplir le champs 'mot de passe'.",
+            "errorType": "InvalidField",
+            "field": "mdp"
+        };
+        response.status(500).send(error);
+        return;
+    }
     const newClient = new Client({
         "nom": nom,
         "prenom": prenom,
